22ba7d955585162b3036710ce84c3a879a59f267
sgourav <sgourav@vmware.com>
2023-07-28 15:37:39 +0530

    Updated the flask app to include exception handling

--- play-with-json/flask_app.py
+++ play-with-json/flask_app.py
@@ -8,16 +8,17 @@ import requests
 app= Flask(__name__)
 
 @app.route("/")
-def get_data():    
-    url = 'http://jsonplaceholder.typicode.com/users'
-    response = requests.get(url)
-    
-    if response.status_code ==200:
-        data=response.json()
-        return jsonify(data)
-    else:
-        return jsonify("error")
-    response.status_code
+def get_data():
+    try:
+        url = 'http://jsonplaceholder.typicode.com/users'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data=response.json()
+            return jsonify(data)
+        else:
+            return jsonify(f"Error: Failed to fetch data from {url}, Status Code: {response.status_code}")
+    except Exception as e:
+        return f"Error: {str(e)}"
     
 if __name__=='__main__':
     app.run(debug=True)
