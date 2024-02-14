e69a586ac7db01883e958e5182b8d76767bf204a
sgourav <sgourav@vmware.com>
2024-02-14 15:40:07 +0530

    Updated string-to-list-convertor to show total entries and display the list in batches of 20 Updated Youtube-Video-Downloader to: 1. Accept the input from an input file 2. Added a new file to input the data

--- Youtube-Video-Downloader/youtube_video_downloader.py
+++ Youtube-Video-Downloader/youtube_video_downloader.py
@@ -1,14 +1,21 @@
 # Importing the necessary libraries
 from pytube import YouTube
-from google.colab import drive
+# from google.colab import drive
 
 # Steps in google colab to integrate with Google Drive
-drive.mount('/content/gdrive')
-%cd 'gdrive/My Drive/Rush_Classes/C3 Batch 26'
+# drive.mount('/content/gdrive')
+# %cd 'gdrive/My Drive/Rush_Classes/C3 Batch 26'
+
+# Accept the input and format in the right format
+def accept_format_input():
+    myl = []
+    with open("input.txt", "r") as data:
+        myl = list(data.read().split('\n'))
+    return myl
 
 # Function to start downloading the video
 def startDownload(url):
-    
+
     try:
 
         # Creating the YouTube object with given URL
@@ -27,7 +34,7 @@ def startDownload(url):
         print('Video {} size: {} mb'.format(title, file_size))
         
         # Downloading the video at specified location
-        # stream.download(download_path)
+        stream.download(download_path)
         print('Video {} downloaded successfully!'.format(title))
 
     except Exception as e:
@@ -37,11 +44,14 @@ def startDownload(url):
 # The driver code.
 if __name__ == '__main__':
     
-    # urls = ['https://youtu.be/anqsgNzCJPM', 'https://youtu.be/IHOSIgqQOIg', 'https://youtu.be/IrJeyYA-LVQ', 'https://youtu.be/mKXgf9Py0SA', 'https://youtu.be/IWBCVzvLiuk', 'https://youtu.be/ZIJsjAhV1Cc', 'https://youtu.be/em40jhBG8a8', 'https://youtu.be/pvOYqSZmvBI', 'https://youtu.be/U8eNks6Wkvs', 'https://youtu.be/lz6CN0r3KK8', 'https://youtu.be/2nYcaaKYL6k', 'https://youtu.be/ur9w_axUP2w', 'https://youtu.be/9JHGZWsxdCA', 'https://youtu.be/s1EwUZB3XuI', 'https://youtu.be/WD0LGV1afak', 'https://youtu.be/OgMxRniPmgE', 'https://youtu.be/rmfWSu9b3Qs', 'https://youtu.be/g1oHqw891qY', 'https://youtu.be/wyqnu9wZwkY', 'https://youtu.be/o50E_m_WSj0', 'https://youtu.be/oVLkXOOJEjo', 'https://youtu.be/XuvY_anz8OI', 'https://youtu.be/5iMGGk1Psh0', 'https://youtu.be/Y4-e1Aie6xA', 'https://youtu.be/lCdtCnM03Hs', 'https://youtu.be/pmVtpEHCbyg', 'https://youtu.be/Y7iCSUsqLQg', 'https://youtu.be/EA6R5qDNBP0', 'https://youtu.be/l-_1To9OWIY', 'https://youtu.be/GoL5MEdiChs', 'https://youtu.be/JoZZTzQugfM', 'https://youtu.be/6AKWoKppHog', 'https://youtu.be/yumYq912zh0', 'https://youtu.be/7raaMBo7_7Y']
-    urls = ['https://youtu.be/anqsgNzCJPM']
+    # Take input from the input file
+    urls = accept_format_input()
     
+    # Display number of videos to be downloaded from youtube
     n = len(urls)
+    print('Total videos to be downloaded:', n)
 
+    # Download the videos one-by-one
     for i in range(n):
-
+        
       startDownload(urls[i])
--- string-to-list-convertor/app.py
+++ string-to-list-convertor/app.py
@@ -2,4 +2,8 @@ myl = []
 with open("input.txt", "r") as data:
     myl = list(data.read().split('\n'))
     myl.sort()
-    print(myl)
+    print(f"Total number of strings: {len(myl)}")
+    for i in range(1, len(myl)+1):
+        print("'", myl[i-1], "'", sep = "", end=', ')
+        if i%20 == 0:
+            print()
